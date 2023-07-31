e872ea63ebf6ed9535ec6bfec9ab30179ddf8f0e
Byeong Min Cho <merrybmc@gmail.com>
2023-07-31 17:04:40 +0900

    [#1] 정글시네마 과제 구현

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Mainpage from './pages/Mainpage';
+
+function App() {
+  return (
+    <>
+      <Mainpage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../components/main/main/Main';
+import Footer from '../components/footer/Footer';
+import './Mainpage.scss';
+
+export default function Mainpage() {
+  return (
+    <div className='container'>
+      <Main />
+      <Footer />
+    </div>
+  );
+}

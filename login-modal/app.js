8f1446a48e782ede750b3c59fadcce0026aeb1ce
timetam <timetam24@naver.com>
2023-08-08 00:52:28 +0900

    [#11] Fix: "아이디와 비밀번호 불일치 안내 메세지 오류 수정"
    1. 로그인 성공 후 불일치 안내 메세지가 삭제되지 않는 오류 수정
    2. 아이디 미입력 시 기존의 불일치 안내 메세지가 삭제되지 않고 아이디 입력 안내 메세지와 동시 출력되는 오류 수정

--- login-modal/app.js
+++ login-modal/app.js
@@ -22,7 +22,10 @@ const loginBtnHandler = (event) => {
     }
   };
 
-  if (!userId.value.trim()) {
+  if (userId.value === "weniv07" && userPwd.value === "frontend07!") {
+    alert("로그인 성공!");
+    secondErrorMsg ? removeErrorMsg(secondErrorMsg) : null;
+  } else if (!userId.value.trim()) {
     // 아이디 미입력 시
     userId.classList.add("focus");
     createErrorMsg(
@@ -31,6 +34,7 @@ const loginBtnHandler = (event) => {
       "아이디를 입력해주세요.",
       userPwd
     );
+    secondErrorMsg ? removeErrorMsg(secondErrorMsg) : null;
     return;
   } else if (userId.value.trim() && !userPwd.value.trim()) {
     // 비밀번호 미입력 시
@@ -44,8 +48,8 @@ const loginBtnHandler = (event) => {
       userPwd.nextSibling
     );
     return;
-  } else if (userId.value !== "weniv07" && userPwd.value !== "frontend07!") {
-    // 아이디와 비밀번호 불일치 시
+  } else if (userId.value !== "weniv07" || userPwd.value !== "frontend07!") {
+    // 아이디 또는 비밀번호 불일치 시
     userPwd.classList.remove("focus");
     createErrorMsg(
       secondErrorMsg,
@@ -53,9 +57,6 @@ const loginBtnHandler = (event) => {
       "아이디 혹은 비밀번호가 일치하지 않습니다.",
       userPwd.nextSibling
     );
-  } else {
-    removeErrorMsg(secondErrorMsg);
-    alert("로그인 성공!");
   }
 };
 
